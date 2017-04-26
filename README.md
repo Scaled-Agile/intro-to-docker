9d46c20ef1ae6352f40ca103e60f5927faf4c432
Yongmin Xia <Jason.Yongmin@gmail.com>
2017-04-26 10:23:40 +1000

    Init commit. Added setup script

---
+++ README.md
@@ -0,0 +1,16 @@
+# Introduction to docker
+
+This is Reece internal Docker traning material.
+
+1. Install Docker & Docker Compose
+
+  - [Docker for Mac](https://docs.docker.com/docker-for-mac/)
+  - [Docker for Windows](https://docs.docker.com/docker-for-windows/)
+
+2. Clone this repository
+
+  ```
+  git clone ssh://git@stash.reecenet.org:7999/train/intro-to-docker.git
+  ```
+
+3. Run `./setup.sh`
---
+++ setup.sh
@@ -0,0 +1,79 @@
+#! /bin/bash
+
+yay() {
+  echo -ne "\033[32mYAY: \033[0m"; echo "$1"
+}
+
+boo() {
+  (echo -ne "\033[31mBOO: \033[0m"; echo "$1") >&2
+  exit 1
+}
+
+umm() {
+  (echo -ne "\033[33mUMM: \033[0m"; echo "$1") >&2
+}
+
+beat() {
+  echo ""
+}
+
+docker_sock=/var/run/docker.sock
+if test -n "$DOCKER_HOST"; then
+  yay "\$DOCKER_HOST is set"
+  docker_addr=$(echo $DOCKER_HOST | sed -e 's|tcp://\(.*\):[0-9]*|\1|')
+  if test -e $docker_sock; then
+    umm "$docker_sock exists, too - that's weird"
+    echo "     Beware of confusion between Docker for Mac and docker-machine!"
+  fi
+elif test -e $docker_sock; then
+  yay "$docker_sock exists"
+  docker_addr=localhost
+else
+  boo "No \$DOCKER_HOST, no $docker_sock ... where's Docker?"
+fi
+
+if docker info > /dev/null; then
+  yay "I can talk to docker"
+else
+  boo "I can't talk to docker"
+fi
+
+beat
+
+echo "Docker is at $docker_addr"
+
+beat
+
+version_of() {
+  $1 --version | egrep -o '[0-9]+(\.[0-9]+)+'
+}
+
+version_lte() {
+  [ "$1" = "`echo -e "$1\n$2" | docker run -i debian sort -V | head -n1`" ]
+}
+
+check_version() {
+  app=$1
+  expected_version=$2
+  actual_version=$(version_of $app)
+  if version_lte $expected_version $actual_version; then
+    yay "$app version $actual_version"
+  else
+    boo "need at least $app version $expected_version, got $actual_version"
+  fi
+}
+
+check_version docker 1.12.5
+check_version docker-compose 1.9.0
+
+beat
+
+pull() {
+  echo ""
+  echo "=== $1"
+  docker pull $1
+}
+
+echo "Pulling some images to get you started ..."
+pull ubuntu
+
